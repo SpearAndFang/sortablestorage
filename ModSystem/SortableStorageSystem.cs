c82f88d0ee6720ba933db3f89a019b81b52f5bc3
_Kristof_ <krizziewissie@gmail.com>
2025-04-08 23:22:22 +0200

    Replaced custom copy of ItemPileable with harmony patch for compatibility Changed other harmony patch to match same format (for my personal sanity, or what's left of it anyway)

--- ModSystem/SortableStorageSystem.cs
+++ ModSystem/SortableStorageSystem.cs
@@ -8,24 +8,16 @@ namespace SortableStorage.ModSystem
 
     public class SortableStorageSystem : ModSystem
     {
-        private readonly Harmony harmony = new Harmony("com.spearandfang.sortablestorage");
+        const string harmonyId = "com.spearandfang.sortablestorage";
+
+        private readonly Harmony harmony = new Harmony(harmonyId);
 
         public override bool ShouldLoad(EnumAppSide forSide)
         { return true; }
 
-        public override void StartClientSide(ICoreClientAPI api)
-        {
-            base.StartClientSide(api);
-            var SSBlockGetPlacedBlockInfoOriginal = typeof(Block).GetMethod(nameof(Block.GetPlacedBlockInfo));
-            var SSBlockGetPlacedBlockInfoPostfix = typeof(SS_BlockGetPlacedBlockInfo_Patch).GetMethod(nameof(SS_BlockGetPlacedBlockInfo_Patch.SSBlockGetPlacedBlockInfoPostfix));
-            this.harmony.Patch(SSBlockGetPlacedBlockInfoOriginal, postfix: new HarmonyMethod(SSBlockGetPlacedBlockInfoPostfix));
-
-        }
-
         public override void Dispose()
         {
-            var SSBlockGetPlacedBlockInfoOriginal = typeof(Block).GetMethod(nameof(Block.GetPlacedBlockInfo));
-            this.harmony.Unpatch(SSBlockGetPlacedBlockInfoOriginal, HarmonyPatchType.Postfix, "*");
+            harmony.UnpatchAll(harmonyId);
             base.Dispose();
         }
 
@@ -53,6 +45,9 @@ namespace SortableStorage.ModSystem
         public override void Start(ICoreAPI api)
         {
             base.Start(api);
+
+            if (!Harmony.HasAnyPatches(harmonyId)) harmony.PatchAllUncategorized();
+
             api.World.Logger.Event("started 'Sortable Storage' mod");
             api.RegisterBlockClass("BlockGenericSortableTypedContainer", typeof(BlockGenericSortableTypedContainer));
             api.RegisterBlockClass("BlockSortableLabeledChest", typeof(BlockSortableLabeledChest));
@@ -63,26 +58,6 @@ namespace SortableStorage.ModSystem
             api.RegisterBlockEntityClass("BEGenericSortableTypedContainer", typeof(BEGenericSortableTypedContainer));
             api.RegisterBlockEntityClass("BESortableLabeledChest", typeof(BESortableLabeledChest));
             api.RegisterBlockEntityClass("BESortableOpenableContainer", typeof(BESortableOpenableContainer));
-
-            api.RegisterItemClass("ItemCoalPatched", typeof(ItemCoalPatched));
-            api.RegisterItemClass("ItemPileablePatched", typeof(ItemPileablePatched));
-        }
-
-
-        public class SS_BlockGetPlacedBlockInfo_Patch
-        {
-            [HarmonyPostfix]
-            public static void SSBlockGetPlacedBlockInfoPostfix(ref string __result, IPlayer forPlayer)
-            {
-                var domain = forPlayer.Entity?.BlockSelection?.Block?.Code?.Domain;
-                if (domain != null)
-                {
-                    if (domain == "sortablestorage")
-                    {
-                        __result += "\n\n<font color=\"#E8DAEF\"><i>Sortable Storage</i></font>\n\n";
-                    }
-                }
-            }
         }
 
     }
